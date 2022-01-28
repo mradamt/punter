4c546e0b8f304476152078c7940efae7891fe249
Adam Tyler <mr.adamtyler@gmail.com>
2022-01-28 14:26:44 -0800

    install sass, classnames

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+import './Content.scss'
+
 export default function Content (props: any) {
 
 
   return (
-    <>
-      I AM CONTENT
+    <div className="content">
+      <p className="content-text">
+        I AM CONTENT
+      </p>
       {props.content}
-    </>
+    </div>
   )
 }
