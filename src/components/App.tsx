d118142997c8ee00456b3c989ea0860d9e252872
Adam Tyler <mr.adamtyler@gmail.com>
2022-01-29 16:47:49 -0800

    add form component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import Post from './Post';
+import Form from './Form';
 import './sass-styles/App.scss';
 
 function App() {
   return (
     <main className='App'>
       <section className='submission-form'>
-        Submission form
+        <Form />
+      </section>
+      <section className='filter-posts'>
+        Filters
       </section>
       <section className='posts'>
         <Post />
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './sass-styles/Form.scss'
+
+export default function Form (props:any) {
+  return (
+    <section className='form'>
+      <form action="">
+        IAMA form
+      </form>
+    </section>
+  )
+}
