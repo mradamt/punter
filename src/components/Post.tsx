a73e718bfeb0135e31b556e1ed68414354ecb8f3
Adam Tyler <mr.adamtyler@gmail.com>
2022-01-30 15:37:48 -0800

    link db data for Tags component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,13 +3,11 @@ import Content from './Content';
 import Tags from './Tags';
 import './sass-styles/Post.scss'
 
-const dummyTags = ["tag a", "tag b"]
-
 export default function Post (props:any) {
   return (
     <div className='post'>
       <Content text={props.text} />
-      <Tags tags={dummyTags} />
+      <Tags tags={props.tags} />
       <div className='author'>
         <span>
           Posted by '{props.author}' on '{props.timestamp}'
