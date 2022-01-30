ff2e4ac09e3221a31753d944c172b85f7ae90aac
Adam Tyler <mr.adamtyler@gmail.com>
2022-01-30 15:29:42 -0800

    correct posts.map so posts render (still using placeholders)

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -4,15 +4,13 @@ import './sass-styles/Posts.scss';
 
 export default function Posts(props:any) {
   // render Post component for every post in props.posts
-  const posts = () => {
-    props.map((post:any, index:number) => <Post 
-      key={index}    
-      text={post.content.text}
-      tags={post.tags}
-      author={post.created_by.author}
-      timestamp={post.created_by.timestamp}
-    />)
-  }
+  const posts = props.posts.map((post:any, index:number) => <Post 
+    key={index}    
+    text={post.content.text}
+    tags={post.tags}
+    author={post.created_by.author}
+    timestamp={post.created_by.timestamp}
+  />)
 
   return (
     <section className='posts'>
