a74a11f4956e14a88be4ec3968ae965de3390418
Adam Tyler <mr.adamtyler@gmail.com>
2022-02-24 19:39:27 -0800

    clear form on post submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './sass-styles/Form.scss'
 
 export default function Form (props) {
-  const [text, setText] = useState()
+  const [text, setText] = useState('')
 
   const formatPost = (text, template, tags, author) => {
      return (
@@ -26,21 +26,22 @@ export default function Form (props) {
     // validate post
     // verify post
     // execute 'add post' function to add to main state
-    props.savePost(formatPost(text, "templaceholder", ['mytag1', 'mytag2'], 'John Authorson'))
+    props.savePost(formatPost(text, 'templaceholder', ['mytag1', 'mytag2'], 'John Authorson'))
+    setText('')
   }
 
   return (
     <section className='submission-form'>
-      <form action="POST" onSubmit={submitPost}>
-        <div className="submission-form-text">
+      <form action='/posts' method='POST' onSubmit={submitPost}>
+        <div className='submission-form-text'>
           <input 
             value={text} 
             onChange={event => setText(event.target.value)} 
-            placeholder="If [blank] doesn't exist" 
+            placeholder="If [blank] doesn't exist"
           />
           <span>, then capitalism has failed us</span>
         </div>
-        <button type="submit">Post</button>
+        <button type='submit'>Post</button>
       </form>
     </section>
   )
