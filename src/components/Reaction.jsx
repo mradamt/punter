f1b43a2a151fe98e812c44820f420c544ad34f78
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-01 16:43:30 -0800

    basic alignment of post's css

--- src/components/Reaction.jsx
+++ src/components/Reaction.jsx
@@ -4,8 +4,8 @@ import './sass-styles/Reaction.scss';
 export default function Reaction (props) {
   return (
     <div className='reaction'>
-      <div>{props.count}-</div>
-      <div>{props.icon}-</div>
+      <div>{props.count}</div>
+      <div>{props.icon}</div>
       <div>{props.label}</div>
     </div>
   )
