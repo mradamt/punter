82cc448c18a897d8374caa82259c7c350a9a53c8
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-23 13:56:28 -0700

    store latest data-from-db format in new json file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import db from '../fauxdb.json'
+import db from '../dataFromDB.json'
 
 
 export default function useApplicationData() {
