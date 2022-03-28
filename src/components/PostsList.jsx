ad5af18ef5186c14c9c3cb7005ad62b61303a1c2
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-28 14:19:31 -0700

    postsList to reference data correctly in new structure

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,14 +3,14 @@ import './sass-styles/Posts.scss';
 
 export default function PostsList(props) {
   // Add reaction types and handleReactionCount function to each PostItem component
-  const postItemArray = props.postsList.map((post, postItemIndex) => {
+  const postItemArray = props.postsList.map((post) => {
     return (
       <PostItem
-        key={postItemIndex}
+        key={post.id}
         post={post}
         reactionTypes={props.reactionTypes}
         handleReactionCount={props.handleReactionCount}
-        // prompts={props.prompts}
+        prompt={props.prompts[post.prompt.id]}
       />
     )
   })
