ef89d64dd93e0a33d7f95f84824c9e7f2c102638
Adam Tyler <mr.adamtyler@gmail.com>
2022-03-28 14:28:28 -0700

    improve postItem formatting for readability

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,27 +2,30 @@ import Content from './Content';
 import Reaction from './Reaction';
 import './sass-styles/Post.scss'
 
-export default function Post ({post, reactionTypes, handleReactionCount, prompt}) {
-  // calculateAge accepts timestamp as milliseconds or new Date() object, returns age in days
+export default function Post (props) {
+  const {post, reactionTypes, handleReactionCount, prompt} = props;
+
+  // Calc age (days) of new Date() object or time in milliseconds
   const calculateAge = ms => Math.floor((Date.now() - ms) / 1000 / 60 / 60 / 24)
 
   // Combine reaction counts and types into an array of Reaction components
-  const reactions = Object.entries(post.reaction_counts).map(([id, count]) => {
-    if (!reactionTypes[id]) {
-      console.log(`Error: reaction id '${id}' not a valid index of reaction_types`);
-      return "<div>Error</div>"
-    }
-    return (
-      <Reaction
-        key={id}
-        count={count} 
-        icon={reactionTypes[id].icon}
-        label={reactionTypes[id].label}
-        onClick={() => handleReactionCount(post, id)}
-        isUserReaction={post.user_reaction_index===id}
-      />
-    )
-  })
+  const reactions = Object.entries(post.reaction_counts)
+    .map(([id, count]) => {
+      if (!reactionTypes[id]) {
+        console.log(`Error: reaction id '${id}' not a valid index of reaction_types`);
+        return "<div>Error</div>"
+      }
+      return (
+        <Reaction
+          key={id}
+          count={count} 
+          icon={reactionTypes[id].icon}
+          label={reactionTypes[id].label}
+          onClick={() => handleReactionCount(post, id)}
+          isUserReaction={post.user_reaction_index===id}
+        />
+      )
+    })
 
   return (
     <div className='post'>
