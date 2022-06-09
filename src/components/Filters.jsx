f6778f906f15230b8444bd55fb3caa68ca191d68
Adam Tyler <mr.adamtyler@gmail.com>
2022-06-09 13:07:03 -0700

    style upgrades: filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,14 +2,15 @@ import './sass-styles/Filters.scss'
 
 export default function Filters (props) {
   return (
-    <section className='filter-posts'>
-      Include mature language:
-      <input 
-        type='checkbox' 
-        onClick={() => props.handleFilters()}
-        checked={props.filters.showSpicyLanguage}
-        />
-
-    </section>
+    <div className='filters-area'>
+      <section className='filters'>
+        Spicy language:
+        <input 
+          type='checkbox' 
+          onClick={() => props.handleFilters()}
+          checked={props.filters.showSpicyLanguage}
+          />
+      </section>
+    </div>
   )
 }
